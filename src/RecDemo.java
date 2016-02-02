88a424cc9132e004be153bc657ffa1ce87b1d1a4
JonathanThomas <JonathanThomas@10.129.51.29>
2016-02-01 22:14:02 -0500

    File /Users/JonathanThomas/Documents/Sophmore year/Spring 2016/CPSC 270 new/Assignment 1/src/RecDemo.java changed at Mon Feb 01 22:14:02 EST 2016

---
+++ src/RecDemo.java
@@ -0,0 +1,47 @@
+
+public class RecDemo extends java.lang.Object{
+	
+	/**
+	 * Do not allow instantiation of this class
+	 */
+	public RecDemo(){
+		
+	}
+	
+	/**
+	 * Calculate factorial of given parameter. 0! = 1
+	 * @param n - is the int whose factorial is to be computed. n is never negative.
+	 * @return n!
+	 */
+	public static int factorial(int n){
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @param n - is the int whose fibonacci sequence is to be computed. n is never negative
+	 * @return fibonacci number for n.
+	 */
+	public static int fibonacci(int n){
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @param s - is string to analyze
+	 * @return true if s is palindrome, false else
+	 */
+	public static boolean isPal(java.lang.String s){
+		return true;
+	}
+	
+	/**
+	 * Create a palidrome of the form "xxx$xxx" where the 
+	 * number of x's before and after the $ are equal to the given parameter.
+	 * @param n - number of x's on each side of the $. n is never negative
+	 * @return the palindrome
+	 */
+	public static java.lang.String mkPal(int n){
+		return "";
+	}
+}
