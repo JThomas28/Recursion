17e7a8c9dc97b0902adb4d47e409eef055808811
JonathanThomas <JonathanThomas@10.129.51.29>
2016-02-04 14:55:55 -0500

    File /Users/JonathanThomas/Documents/Sophmore year/Spring 2016/CPSC 270/Assignment 1/src/RecDemo.java changed at Thu Feb 04 14:55:55 EST 2016

--- src/RecDemo.java
+++ src/RecDemo.java
@@ -48,7 +48,7 @@ public class RecDemo extends java.lang.Object {
 		if (s.length() == 1) {
 			return true;
 		}
-		else if (s.charAt(0) == s.charAt(s.length())) {
+		else if (s.charAt(0) == s.charAt(s.length()-1)) {
 			// if first letter is same as last
 			return isPal(s.substring(1, s.length() - 1));
 		}
