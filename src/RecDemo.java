9f3b831441a599aede0f5e24a8d13955780f3042
JonathanThomas <JonathanThomas@10.129.51.29>
2016-02-04 15:45:14 -0500

    File /Users/JonathanThomas/Documents/Sophmore year/Spring 2016/CPSC 270/Assignment 1/src/RecDemo.java changed at Thu Feb 04 15:45:14 EST 2016

--- src/RecDemo.java
+++ src/RecDemo.java
@@ -33,11 +33,14 @@ public class RecDemo extends java.lang.Object {
 	 * @return fibonacci number for n.
 	 */
 	public static int fibonacci(int n) {
-		if(n==1 || n==2){
+		if(n==0){
+			return 0;
+		}
+		else if(n==1 || n==2){
 			return 1;
 		}
 		else{
-			return (fibonacci(n-1));
+			return (fibonacci(n-1) + fibonacci(n-2));
 		}
 	}
 
