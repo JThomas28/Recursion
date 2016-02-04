2275502fece2367ac8f27d7088606c49d065ce9d
JonathanThomas <JonathanThomas@10.129.51.29>
2016-02-04 14:02:02 -0500

    File /Users/JonathanThomas/Documents/Sophmore year/Spring 2016/CPSC 270/Assignment 1/src/RecDemoTest.java changed at Thu Feb 04 14:02:01 EST 2016

---
+++ src/RecDemoTest.java
@@ -0,0 +1,41 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class RecDemoTest {
+
+	@Test
+	public void testRecDemo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFactorial() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFibonacci() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsPal() {
+		//RecDemo recdemo = new RecDemo();
+		Boolean result = RecDemo.isPal("racecar");
+		assertEquals(true, result);
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMkPal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+}
