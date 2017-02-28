4456ef86df9aaa0fcb0ebfca573d0a15fa90dc86
JThomas28 <jonathan.thomas.14@cnu.edu>
2017-02-28 15:53:05 -0500

    Upload recursion project

--- src/RecDemo.java
+++ src/RecDemo.java
@@ -5,85 +5,88 @@
  * @version 2/5/2016
  */
 public class RecDemo {
-	/**
-	 * Do not allow instantiation of constructor
-	 */
-	public RecDemo() {
-		// no instantiation of constructor
-	}
+    /**
+     * Do not allow instantiation of constructor
+     */
+    public RecDemo() {
+        // no code needed here
+    }
 
-	/**
-	 * Calculate factorial of given parameter. 0! = 1
-	 * 
-	 * @param n
-	 *            - is the int whose factorial is to be computed. n is never
-	 *            negative.
-	 * @return n!
-	 */
-	public static int factorial(int n) {
-		if (n == 0) {
-			return 1;
-		}
-		else {
-			return (n * factorial(n - 1));
-		}
-	}
+    /**
+     * Calculate factorial of given parameter. 0! = 1
+     * 
+     * @param n
+     *            is the int whose factorial is to be computed. n is never
+     *            negative.
+     * @return n!
+     */
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        else {
+            return (n * factorial(n - 1));
+        }
+    }
 
-	/**
-	 * Determine the fibonacci number for the given parameter.
-	 * 
-	 * @param n
-	 *            - is the int whose fibonacci sequence is to be computed. n is
-	 *            never negative
-	 * @return fibonacci number for n.
-	 */
-	public static int fibonacci(int n) {
-		if (n == 0) {
-			return 0;
-		}
-		else if (n == 1 || n == 2) {
-			return 1;
-		}
-		else {
-			return (fibonacci(n - 1) + fibonacci(n - 2));
-		}
-	}
+    /**
+     * Determine the fibonacci number for the given parameter.
+     * 
+     * @param n
+     *            is the int whose fibonacci sequence is to be computed. n is
+     *            never negative
+     * @return fibonacci number for n.
+     */
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        else if (n == 1 || n == 2) {
+            return 1;
+        }
+        else {
+            return (fibonacci(n - 1) + fibonacci(n - 2));
+        }
+    }
 
-	/**
-	 * Determine if the given String is a palindrome, by looking only at the
-	 * first and last character of the String.
-	 * 
-	 * @param s
-	 *            - is string to analyze
-	 * @return true if s is palindrome, false else
-	 */
-	public static boolean isPal(java.lang.String s) {
-		if (s.length() == 1) {
-			return true;
-		}
-		else if (s.charAt(0) == s.charAt(s.length() - 1)) {
-			// if first letter is same as last
-			return isPal(s.substring(1, s.length() - 1));
-		}
-		else {
-			return false;
-		}
-	}
+    /**
+     * Determine if the given String is a palindrome, by looking only at the
+     * first and last character of the String.
+     * 
+     * @param s
+     *             is string to analyze
+     * @return true if s is palindrome, false else
+     */
+    public static boolean isPal(java.lang.String s) {
+        if (s.isEmpty()) {
+            return true;
+        }
+        else if (s.length() == 1) {
+            return true;
+        }
+        else if (s.charAt(0) == s.charAt(s.length() - 1)) {
+            // if first letter is same as last
+            return isPal(s.substring(1, s.length() - 1));
+        }
+        else {
+            return false;
+        }
+    }
 
-	/**
-	 * Create a palindrome of the form "xxx$xxx" where the number of x's before
-	 * and after the $ are equal to the given parameter.
-	 * 
-	 * @param n
-	 *            - number of x's on each side of the $. n is never negative
-	 * @return the palindrome
-	 */
-	public static java.lang.String mkPal(int n) {
-		if (n == 0) {
-			return "$";
-		}
-		else {
-			return ("x" + mkPal(n - 1) + "x");
-		}
-	}
+    /**
+     * Create a palindrome of the form "xxx$xxx" where the number of x's before
+     * and after the $ are equal to the given parameter.
+     * 
+     * @param n
+     *            - number of x's on each side of the $. n is never negative
+     * @return the palindrome
+     */
+    public static java.lang.String mkPal(int n) {
+        if (n == 0) {
+            return "$";
+        }
+        else {
+            return ("x" + mkPal(n - 1) + "x");
+        }
+    }
 }
--- src/RecDemoTest.java
+++ src/RecDemoTest.java
@@ -2,94 +2,158 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-
+/**
+ * Test class for recDemo class
+ * 
+ * @author JonathanThomas
+ * @version 2/5/2016
+ */
 public class RecDemoTest {
 
-	@Test
-	public void testFactorialZero() {
-		int expected = 1;
-		int actual = RecDemo.factorial(0);
-		assertEquals("Factorial of 0 is 1", expected, actual);
-	}
-	@Test
-	public void testFactorial1() {
-		int expected = 1;
-		int actual = RecDemo.factorial(1);
-		assertEquals("Factorial of 1 is 1", expected, actual);
-	}
-	@Test
-	public void testFactorialLargeNumber() {
-		int expected = 362880;
-		int actual = RecDemo.factorial(9);
-		assertEquals("Factorial of 9 is 362880", expected, actual);
-	}
+    /**
+     * Tests factorial of 0 is 1.
+     */
+    @Test
+    public void testFactorialZero() {
+        int expected = 1;
+        int actual = RecDemo.factorial(0);
+        assertEquals("Factorial of 0 is 1", expected, actual);
+    }
 
-	@Test
-	public void testFibonacciOfZero() {
-		int expected = 0;
-		int actual = RecDemo.fibonacci(0);
-		assertEquals("Fibonacci of 0 is 0", expected, actual);
-	}
-	@Test
-	public void testFibonacciOfOne() {
-		int expected = 1;
-		int actual = RecDemo.fibonacci(1);
-		assertEquals(expected, actual);
-	}
-	@Test
-	public void testFibonacciOfThree() {
-		int expected = 2;
-		int actual = RecDemo.fibonacci(3);
-		assertEquals(expected, actual);
-	}
-	@Test
-	public void testFibonacciOfBigNumber() {
-		int expected = 34;
-		int actual = RecDemo.fibonacci(9);
-		assertEquals(expected, actual);
-	}
-	
+    /**
+     * Tests factorial of 1 is 1
+     */
+    @Test
+    public void testFactorial1() {
+        int expected = 1;
+        int actual = RecDemo.factorial(1);
+        assertEquals("Factorial of 1 is 1", expected, actual);
+    }
 
-	@Test
-	public void testIsPalTrue() {
-		Boolean actual = RecDemo.isPal("racecar");
-		assertTrue(actual);
-		
-	}
-	@Test
-	public void testIsPalOneLetter() {
-		Boolean actual = RecDemo.isPal("A");
-		assertTrue(actual);
-	}
+    /**
+     * Tests factorial of 9 is 362880
+     */
+    @Test
+    public void testFactorialLargeNumber() {
+        int expected = 362880;
+        int actual = RecDemo.factorial(9);
+        assertEquals("Factorial of 9 is 362880", expected, actual);
+    }
 
-	@Test
-	public void testIsPalNotTrueLongWord() {
-		Boolean actual   = RecDemo.isPal("Hippopotamus");
-		assertFalse( "Hippopotamus is not a Palendrome", actual);
-	}
-	
-	@Test
-	public void testMkPalZero() {
-		String expected = "$";
-		String actual = RecDemo.mkPal(0);
-		assertEquals(expected, actual);
-	}
-	@Test
-	public void testMkPalOne() {
-		String expected = "x$x";
-		String actual = RecDemo.mkPal(1);
-		assertEquals(expected, actual);
-	}
-	@Test
-	public void testMkPalFive() {
-		String expected = "xxxxx$xxxxx";
-		String actual = RecDemo.mkPal(5);
-		assertEquals(expected, actual);
-	}
+    /**
+     * Tests fibonacci of 0 is 0
+     */
+    @Test
+    public void testFibonacciOfZero() {
+        int expected = 0;
+        int actual = RecDemo.fibonacci(0);
+        assertEquals("Fibonacci of 0 is 0", expected, actual);
+    }
 
-	@Test
-	public void testObject() {
-		new RecDemo();
-	}
+    /**
+     * Tests fibonacci of 1 is 1
+     */
+    @Test
+    public void testFibonacciOfOne() {
+        int expected = 1;
+        int actual = RecDemo.fibonacci(1);
+        assertEquals(expected, actual);
+    }
 
+    /**
+     * Tests fibonacci of 3 is 2
+     */
+    @Test
+    public void testFibonacciOfThree() {
+        int expected = 2;
+        int actual = RecDemo.fibonacci(3);
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Tests fibonacci of 9 is 34
+     */
+    @Test
+    public void testFibonacciOfBigNumber() {
+        int expected = 34;
+        int actual = RecDemo.fibonacci(9);
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Tests if racecar is palindrome. Should be true
+     */
+    @Test
+    public void testIsPalTrue() {
+        Boolean actual = RecDemo.isPal("racecar");
+        assertTrue(actual);
+
+    }
+
+    /**
+     * If string is empty, it is not a palindrome
+     */
+    @Test
+    public void testIsPalEmptyString() {
+        Boolean actual = RecDemo.isPal("");
+        assertTrue(actual);
+    }
+
+    /**
+     * Test if single letter is palindrome. Should be true
+     */
+    @Test
+    public void testIsPalOneLetter() {
+        Boolean actual = RecDemo.isPal("A");
+        assertTrue(actual);
+    }
+
+    /**
+     * Tests if Hippopotamus is palindrome. Should be false
+     */
+    @Test
+    public void testIsPalNotTrueLongWord() {
+        Boolean actual = RecDemo.isPal("Hippopotamus");
+        assertFalse("Hippopotamus is not a Palindrome", actual);
+    }
+
+    /**
+     * Tests base case of mkPal. Should return $
+     */
+    @Test
+    public void testMkPalZero() {
+        String expected = "$";
+        String actual = RecDemo.mkPal(0);
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Tests mkPal adds 1 x to each side of $. Should return 'x$x'
+     */
+    @Test
+    public void testMkPalOne() {
+        String expected = "x$x";
+        String actual = RecDemo.mkPal(1);
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Tests mkPal adds 5 x's on either side of $. Should return 'xxxxx$xxxxx'
+     */
+    @Test
+    public void testMkPalFive() {
+        String expected = "xxxxx$xxxxx";
+        String actual = RecDemo.mkPal(5);
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Tests constructor
+     */
+    @Test
+    public void testObject() {
+        RecDemo demo = new RecDemo();
+        RecDemo demo2 = new RecDemo();
+        assertNotSame(demo, demo2);
+    }
 }
